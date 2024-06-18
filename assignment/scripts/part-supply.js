980eb405c60d77fc11284f8fa09875715007e01d
Padee Vang <vangpadee11@gmail.com>
2024-06-17 19:30:42 -0500

    completed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -115,6 +115,13 @@ console.log(stretchZeroes);
 //    array to the 'totalParts' variable.
 console.log('8. Looping through supplyChanges to calculate the sum:');
 
+let totalParts = 0;
+
+for ( let value of supplyChanges) {
+  totalParts += value;
+}
+
+console.log('testing total parts', totalParts);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. There are 572
